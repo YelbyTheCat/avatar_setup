011d037ac8a3bd6773ef48e27e3718ae5543434a
Yelby <41715570+YelbyNoshn@users.noreply.github.com>
2021-01-18 18:08:54 -0500

    Update
    Auto Eye
    Auto Viseme

--- Editor/Avatar_Setup.cs
+++ Editor/Avatar_Setup.cs
@@ -1,10 +1,11 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEditor;
 using UnityEditor.Presets;
 using UnityEditor.SceneManagement;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 
 #if VRC_SDK_VRCSDK3 || VRC_SDK_VRCSDK2
 using VRCSDK2;
@@ -13,8 +14,8 @@ using VRCSDK2;
 public class Avatar_Setup : EditorWindow
 {
     //Attributes
-    string avName = "";
-    GameObject avModel;
+    string avatarName = "";
+    GameObject avatarModel;
     Scene scene;
     
     [MenuItem("Yelby/Avatar Setup")]
@@ -35,46 +36,46 @@ public class Avatar_Setup : EditorWindow
 
         //Get Avatar Name
         EditorGUIUtility.labelWidth = 90;
-        avName = EditorGUILayout.TextField("Avatar Name: ", avName);
+        avatarName = EditorGUILayout.TextField("Avatar Name: ", avatarName);
 
         //Get Avatar Model
         EditorGUILayout.BeginHorizontal();
-        avModel = EditorGUILayout.ObjectField("Avatar FBX: ", avModel, typeof(GameObject), false) as GameObject;
-        if (!AssetDatabase.GetAssetPath(avModel).Contains(".fbx")) { avModel = null; }
+        avatarModel = EditorGUILayout.ObjectField("Avatar FBX: ", avatarModel, typeof(GameObject), false) as GameObject;
+        if (AssetDatabase.GetAssetPath(avatarModel).Contains(".fbx") == false) { avatarModel = null; }
         if (GUILayout.Button("Select"))//Select Avatar
         {
-            avModel = Selection.activeGameObject;
-            if (!AssetDatabase.GetAssetPath(avModel).Contains(".fbx")) { avModel = null; }
+            avatarModel = Selection.activeGameObject;
+            if (AssetDatabase.GetAssetPath(avatarModel).Contains(".fbx") == false) { avatarModel = null; }
         }
         EditorGUILayout.EndHorizontal();
 
-        if (GUILayout.Button("Generate Model"))
+        if (GUILayout.Button("Setup Avatar"))
         {
-            if (avName == "")//Check if name is blank
+            if (avatarName == "")//Check if name is blank
             {
                 Debug.LogWarning("No Name");
                 EditorUtility.DisplayDialog("Avatar Failed", "Name Field is empty!", "Ok");
                 return;
             }
-            else if(AssetDatabase.IsValidFolder("Assets/Avatars/" + avName))//Check if name is taken
+            else if(AssetDatabase.IsValidFolder("Assets/Avatars/" + avatarName) == true)//Check if name is taken
             {
                 Debug.LogWarning("Name Taken");
                 EditorUtility.DisplayDialog("Avatar Failed", "Name Taken!", "Ok");
                 return;
             }
-            if(!avModel == false)//If there is a model it'll run
+            if(!avatarModel == false)//If there is a model it'll run
             {
-                createFolders(avName);
-                moveAvatar(avModel, avName);
-                importSettings(avModel);
-                ExtractMaterials(AssetDatabase.GetAssetPath(avModel), "Assets/Avatars/" + avName + "/UMats/");
-                createScene(avModel, avName);
-                createPrefab(avModel, avName);
-                pullVRCAnims(avName);
-                createSugEyePos(avName);
-                createFloor(avName);
+                createAvatarFolders(avatarName);
+                moveAvatar(avatarModel, avatarName);
+                importSettingsFromPreset(avatarModel);
+                ExtractMaterials(AssetDatabase.GetAssetPath(avatarModel), "Assets/Avatars/" + avatarName + "/UMats/");
+                createScene(avatarModel, avatarName);
+                createAvatarPrefab(avatarModel, avatarName);
+                pullVRCAnimations(avatarName);
+                //createSuggestedEyeLocation(avatarName);
+                createFloor();
                 Debug.Log("Avatar Setup Finished");
-                EditorUtility.DisplayDialog("Avatar Success", "Model Generated\nCheck Assets/Avatars/"+avName, "Ok");
+                EditorUtility.DisplayDialog("Avatar Success", "Model Imported\nCheck Assets/Avatars/"+avatarName, "Ok");
             }
             else
             {
@@ -83,6 +84,7 @@ public class Avatar_Setup : EditorWindow
                 return;
             }
         }
+
         /*if (GUILayout.Button("Check Comps"))
         {
             GameObject selection = Selection.activeGameObject;
@@ -91,34 +93,29 @@ public class Avatar_Setup : EditorWindow
     }
 
     //~~~~Main Methods~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    void createFolders(string avN)
+    void createAvatarFolders(string avatarName)
     {
         //Create Avatars folder
-        if(!AssetDatabase.IsValidFolder("Assets/Avatars"))
+        if(AssetDatabase.IsValidFolder("Assets/Avatars") == false)
         {
             AssetDatabase.CreateFolder("Assets", "Avatars");
-           // Debug.Log("Folder Generated: Avatars");
         }
         //Create "AvatarName" Folder
-        if(!AssetDatabase.IsValidFolder("Assets/Avatars/"+avN))
+        if(AssetDatabase.IsValidFolder("Assets/Avatars/"+avatarName) == false)
         {
-            AssetDatabase.CreateFolder("Assets/Avatars",avN);
-
-            if (AssetDatabase.IsValidFolder("Assets/Avatars/" + avN))
-            { /*Debug.Log("Folder Generated: " + avN);*/}
-            else { Debug.LogError("Folder Generation Failed"); }
+            AssetDatabase.CreateFolder("Assets/Avatars",avatarName);
         }
 
         //Generate Sub-folders
-        string[] subFolders = { "Animations", "FBX", "Prefabs", "Textures", "UMats" };
-        subFolder(subFolders, avN);
+        string[] subFoldersList = { "Animations", "FBX", "Prefabs", "Textures", "UMats" };
+        CreateSubAvatarFolders(subFoldersList, avatarName);
         Debug.Log("Folders Generated");
     }
-    void moveAvatar(GameObject avM, string avN)
+    void moveAvatar(GameObject avatarModel, string avatarName)
     {
-        if(AssetDatabase.IsValidFolder("Assets/Avatars/" + avN + "/FBX"))
+        if(AssetDatabase.IsValidFolder("Assets/Avatars/" + avatarName + "/FBX") == true)
         {
-            AssetDatabase.MoveAsset(AssetDatabase.GetAssetPath(avM), "Assets/Avatars/" + avN + "/FBX/" + avN + ".fbx");
+            AssetDatabase.MoveAsset(AssetDatabase.GetAssetPath(avatarModel), "Assets/Avatars/" + avatarName + "/FBX/" + avatarName + ".fbx");
             Debug.Log("Avatar Moved");
         }
         else
@@ -127,12 +124,12 @@ public class Avatar_Setup : EditorWindow
         }
         AssetDatabase.Refresh();
     }
-    void importSettings(GameObject avM)
+    void importSettingsFromPreset(GameObject avatarModel)
     {
         var preset = AssetDatabase.LoadAssetAtPath<Preset>("Assets/Yelby/Programs/Avatar Setup/YelbyImportPreset.preset");
-        if (preset.CanBeAppliedTo(AssetImporter.GetAtPath(AssetDatabase.GetAssetPath(avM))))
+        if (preset.CanBeAppliedTo(AssetImporter.GetAtPath(AssetDatabase.GetAssetPath(avatarModel))) == true)
         {
-            preset.ApplyTo(AssetImporter.GetAtPath(AssetDatabase.GetAssetPath(avM)));
+            preset.ApplyTo(AssetImporter.GetAtPath(AssetDatabase.GetAssetPath(avatarModel)));
             Debug.Log("Settings Applied");
         }
         else
@@ -140,38 +137,63 @@ public class Avatar_Setup : EditorWindow
             Debug.LogError("Can't Apply Preset");
         }
     }
-    void createScene(GameObject avM, string avN)
+    void createScene(GameObject avatarModel, string avatarName)
     {
         scene = SceneManager.GetActiveScene();
         if (scene.name == "" || scene.name == "SampleScene")
         {
-            addCharacter(avM);
-            EditorSceneManager.SaveScene(scene, "Assets/Avatars/"+avN+"/"+avN+".unity", false);
+            addCharacter(avatarModel);
+            EditorSceneManager.SaveScene(scene, "Assets/Avatars/"+avatarName+"/"+avatarName+".unity", false);
         }
         else
         {
             scene = EditorSceneManager.NewScene(NewSceneSetup.DefaultGameObjects, NewSceneMode.Additive);
-            addCharacter(avM);
-            EditorSceneManager.SaveScene(scene, "Assets/Avatars/" + avN + "/" + avN + ".unity", false);
+            addCharacter(avatarModel);
+            EditorSceneManager.SaveScene(scene, "Assets/Avatars/" + avatarName + "/" + avatarName + ".unity", false);
         }
     }
-    void createPrefab(GameObject avM, string avN)
+    void createAvatarPrefab(GameObject avatarModel, string avatarName)
     {
-        GameObject prefab = GameObject.Find(avN);
+        GameObject prefab = GameObject.Find(avatarName);
 #if VRC_SDK_VRCSDK3
-            prefab.AddComponent<VRC.SDK3.Avatars.Components.VRCAvatarDescriptor>();
+            var avatarDescriptor = prefab.AddComponent<VRC.SDK3.Avatars.Components.VRCAvatarDescriptor>();
+            var eyePosition = createSuggestedEyeLocation(avatarName);
+            avatarDescriptor.ViewPosition = eyePosition;
+            avatarDescriptor.lipSync = VRC.SDKBase.VRC_AvatarDescriptor.LipSyncStyle.VisemeBlendShape;
+            Dictionary<string, GameObject> unpackedBones = UnpackBones(avatarModel, new Dictionary<string, GameObject>());
+            if(unpackedBones.ContainsKey("Body"))
+            {
+                var skinMesh = unpackedBones["Body"].GetComponent<SkinnedMeshRenderer>();
+                List<string> blendshapeNames = new List<string>();
+                blendshapeNames.Add("-none-");
+                for (int i = 0; i < skinMesh.sharedMesh.blendShapeCount; i++)
+                {
+                    var blendName = skinMesh.sharedMesh.GetBlendShapeName(i);
+                    blendshapeNames.Add(blendName);
+                }
+                avatarDescriptor.VisemeSkinnedMesh = skinMesh;
+            if (avatarDescriptor.VisemeBlendShapes == null || avatarDescriptor.VisemeBlendShapes.Length != (int)VRC.SDKBase.VRC_AvatarDescriptor.Viseme.Count)
+                avatarDescriptor.VisemeBlendShapes = new string[(int)VRC.SDKBase.VRC_AvatarDescriptor.Viseme.Count];
+            AutoDetectVisemes(avatarDescriptor, blendshapeNames);
+            }
+            
+            else { Debug.LogError("Body doesn't exist"); }
 #endif
 #if VRC_SDK_VRCSDK2
-        prefab.AddComponent<VRC_AvatarDescriptor>();
+            var AvatarDescriptor = prefab.AddComponent<VRC_AvatarDescriptor>();
+            var eyePosition = createSuggestedEyeLocation(avatarName);
+            AvatarDescriptor.ViewPosition = eyePosition;
+        
 #endif
-        PrefabUtility.SaveAsPrefabAssetAndConnect(prefab, "Assets/Avatars/" + avN + "/Prefabs/" + avN + ".prefab", InteractionMode.AutomatedAction);
+        PrefabUtility.SaveAsPrefabAssetAndConnect(prefab, "Assets/Avatars/" + avatarName + "/Prefabs/" + avatarName + ".prefab", InteractionMode.AutomatedAction);
+        AssetDatabase.Refresh();
         Debug.Log("Prefab Created");
     }
-    void pullVRCAnims(string avN)
+    void pullVRCAnimations(string avatarName)
     {
 #if VRC_SDK_VRCSDK3
-        string folder = "Assets/VRCSDK/Examples3/Animation/Controllers/";
-        string[] oCont =
+        string VRCFolderLocation = "Assets/VRCSDK/Examples3/Animation/Controllers/";
+        string[] originalController =
         {
             "vrc_AvatarV3LocomotionLayer",  //Base
             "vrc_AvatarV3ActionLayer",      //Action
@@ -180,7 +202,7 @@ public class Avatar_Setup : EditorWindow
             "vrc_AvatarV3UtilityTPose",     //TPose
             "vrc_AvatarV3UtilityIKPose"     //IK Pose
         };
-        string[] mCont =
+        string[] movedController =
         {
             "Base (Locomotion)",            //Base
             "Action (Major Animations)",    //Action
@@ -189,33 +211,35 @@ public class Avatar_Setup : EditorWindow
             "TPose",                        //TPose
             "IKPose"                        //IK Pose
         };
-        for(int i = 0;i<oCont.Length;i++)
+        for(int i = 0;i<originalController.Length;i++)
         {
-            AssetDatabase.CopyAsset(folder + oCont[i] + ".controller",
-                "Assets/Avatars/"+avN+"/Animations/VRC_Controllers/"+mCont[i]+"_"+avN+".controller");
+            AssetDatabase.CopyAsset(VRCFolderLocation + originalController[i] + ".controller",
+                "Assets/Avatars/"+avatarName+"/Animations/VRC_Controllers/"+movedController[i]+"_"+avatarName+".controller");
         }
 #endif
 #if VRC_SDK_VRCSDK2
         AssetDatabase.CopyAsset("Assets/VRChat Examples/Examples2/Animation/SDK2/CustomOverrideEmpty.overrideController",
-            "Assets/Avatars/"+avN+"/Animations/VRC_Controllers/"+avN+"_controller.overrideController");
+            "Assets/Avatars/"+avatarName+"/Animations/VRC_Controllers/"+ avatarName + "_controller.overrideController");
         AssetDatabase.CopyAsset("Assets/VRChat Examples/Examples2/Animation/SDK2/CustomOverrideEmpty.overrideController",
-            "Assets/Avatars/" + avN + "/Animations/VRC_Controllers/"+"Sit_" + avN + "_controller.overrideController");
+            "Assets/Avatars/" + avatarName + "/Animations/VRC_Controllers/"+"Sit_" + avatarName + "_controller.overrideController");
 #endif
     }
-    void createSugEyePos(string avN)
+    Vector3 createSuggestedEyeLocation(string avatarName)
     {
-        var eyeBone = GameObject.Find(avN + "/Armature/Hips/Spine/Chest/Neck/Head/Eye_L");
+        var eyeBone = GameObject.Find(avatarName + "/Armature/Hips/Spine/Chest/Neck/Head/Eye_L");
         if(eyeBone == true)
         {
-            GameObject eyeLoc = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            eyeLoc.name = "Suggested Eye Location";
-            eyeLoc.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+            //GameObject eyeLocation = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            //eyeLocation.name = "Suggested Eye Location";
+            //eyeLocation.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
             Vector3 eyeLocPos = new Vector3(0, eyeBone.transform.position.y, eyeBone.transform.position.z);
-            eyeLoc.transform.position = eyeLocPos;
-            EditorSceneManager.SaveScene(scene);
+            //eyeLocation.transform.position = eyeLocPos;
+            //EditorSceneManager.SaveScene(scene);
+            return eyeLocPos;
         }
+        return Vector3.zero;
     }
-    void createFloor(string avN)
+    void createFloor()
     {
         GameObject floor = GameObject.CreatePrimitive(PrimitiveType.Plane);
         floor.name = "Floor";
@@ -230,34 +254,97 @@ public class Avatar_Setup : EditorWindow
         }
     }*/
     //~~~~Helper Methods~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    void subFolder(string[] subN, string avN)
+    void CreateSubAvatarFolders(string[] subN, string avatarName)
     {
         for(int i = 0;i<subN.Length;i++)
         {
-            if (!AssetDatabase.IsValidFolder("Assets/Avatars/" + avN + "/" + subN[i]))
+            if (AssetDatabase.IsValidFolder("Assets/Avatars/" + avatarName + "/" + subN[i]) == false)
             {
-                AssetDatabase.CreateFolder("Assets/Avatars/" + avN, subN[i]);
+                AssetDatabase.CreateFolder("Assets/Avatars/" + avatarName, subN[i]);
 
-                if (AssetDatabase.IsValidFolder("Assets/Avatars/" + avN + "/" + subN[i])){/*Debug.Log("SubFolder Generated: " + subN[i]);*/}
+                if (AssetDatabase.IsValidFolder("Assets/Avatars/" + avatarName + "/" + subN[i]) == true){ }
                 else { Debug.LogError("Sub Folder: " + subN[i] + " Failed"); }
             }
         }
-        if(AssetDatabase.IsValidFolder("Assets/Avatars/"+avN+"/Animations"))
+        if(AssetDatabase.IsValidFolder("Assets/Avatars/"+avatarName+"/Animations") == true)
         {
-            Debug.LogWarning("It Does exist");
-            AssetDatabase.CreateFolder("Assets/Avatars/" + avN + "/Animations", "VRC_Controllers");
-            if(AssetDatabase.IsValidFolder("Assets/Avatars/"+avN+"/Animations/VRC_Controllers"))
-            {
-                Debug.Log("It did a thing");
-            }
+            AssetDatabase.CreateFolder("Assets/Avatars/" + avatarName + "/Animations", "VRC_Controllers");
         }
     }
-    void addCharacter(GameObject avM)
+    void addCharacter(GameObject avatarModel)
     {
-        var av = Instantiate(avM);
-        av.name = avM.name;
+        var avModel = Instantiate(avatarModel);
+        avModel.name = avatarModel.name;
     }
+    Dictionary<string, GameObject> UnpackBones(GameObject bone, Dictionary<string, GameObject> targetBones)
+    {
+        foreach (Transform child in bone.transform)
+        {
+            targetBones.Add(child.name, child.gameObject);
+            targetBones = UnpackBones(child.gameObject, targetBones); //Recursion
+        }
+        return targetBones;
+    }
+    void AutoDetectVisemes(VRC.SDK3.Avatars.Components.VRCAvatarDescriptor avatarDescriptor, List<string> blendShapeNames)
+    {
+        // prioritize strict - but fallback to looser - naming and don't touch user-overrides
 
+        List<string> blendShapes = new List<string>(blendShapeNames);
+        blendShapes.Remove("-none-");
+
+        for (int v = 0; v < avatarDescriptor.VisemeBlendShapes.Length; v++)
+        {
+            if (string.IsNullOrEmpty(avatarDescriptor.VisemeBlendShapes[v]))
+            {
+                string viseme = ((VRC.SDKBase.VRC_AvatarDescriptor.Viseme)v).ToString().ToLowerInvariant();
+
+                foreach (string s in blendShapes)
+                {
+                    if (s.ToLowerInvariant() == "vrc.v_" + viseme)
+                    {
+                        avatarDescriptor.VisemeBlendShapes[v] = s;
+                        goto next;
+                    }
+                }
+                foreach (string s in blendShapes)
+                {
+                    if (s.ToLowerInvariant() == "v_" + viseme)
+                    {
+                        avatarDescriptor.VisemeBlendShapes[v] = s;
+                        goto next;
+                    }
+                }
+                foreach (string s in blendShapes)
+                {
+                    if (s.ToLowerInvariant().EndsWith(viseme))
+                    {
+                        avatarDescriptor.VisemeBlendShapes[v] = s;
+                        goto next;
+                    }
+                }
+                foreach (string s in blendShapes)
+                {
+                    if (s.ToLowerInvariant() == viseme)
+                    {
+                        avatarDescriptor.VisemeBlendShapes[v] = s;
+                        goto next;
+                    }
+                }
+                foreach (string s in blendShapes)
+                {
+                    if (s.ToLowerInvariant().Contains(viseme))
+                    {
+                        avatarDescriptor.VisemeBlendShapes[v] = s;
+                        goto next;
+                    }
+                }
+            next: { }
+            }
+
+        }
+
+        //shouldRefreshVisemes = false;
+    }
     //~~~~Online Helpers~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     public static void ExtractMaterials(string assetPath, string destinationPath)
     {
